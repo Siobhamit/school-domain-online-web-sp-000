08c2d30dab3d83b0a8772426d9fe0c9a7b403df2
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-06-11 01:05:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+attr_reader :name, :roster, :student
+
+
+def initialize(name)
+@name = name
+def roster
+roster = Hash.new
+
+end
+end
+
+# class Student
+#   attr_accessor :name, :grade
+#
+#
+#   def initialize(name, grade)
+#     @name = name
+#     @grade = grade
+# end
+# end
+
+def add_student(name, grade)
+    if roster.key?(grade)
+      roster[grade] << name
+    else
+    roster[grade] = Array.new
+    roster[grade].push name
+ end
+ end
+
+end
